2ffee056c70612583e20b99ae72346322c167ca8
Andres <andruix93@gmail.com>
2024-02-01 22:36:44 -0600

    chore: structure of service!

--- src/server.js
+++ src/server.js
@@ -1,17 +1,24 @@
 const express = require('express');
+const path = require('path');
 
 // Initializations
 const app = express();
 
 // Settings
 app.set('port', process.env.PORT || 4001)
+app.set('views', path.join(__dirname, "views"))
 
 // Middlewares
+app.use(express.urlencoded({ extended: false }));
 
 // Global Variables
 
 // Routes
+app.get('/', (req, res) => {
+  res.send("hello world");
+});
 
 // Static Files
+app.use(express.static(path.join(__dirname, 'public')));
 
 module.exports = app
