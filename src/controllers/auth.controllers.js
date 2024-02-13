bba77f143330cc61cded6798bc58cc1f51ca4cca
Andres <andruix93@gmail.com>
2024-02-13 11:04:29 -0600

    feat: basic crud enpdoints for users

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -2,32 +2,27 @@ import User from "../models/users.js"
 
 export const signup = async (req, res) => {
   const { name, email, password, confirmPassword } = req.body;
+  let errors = [];
   if (password !== confirmPassword) {
-    res.status(400).json({
-      status: "unsuccesful",
-      message: "passwords don't match"
-    });
-    return;
+    errors.push({ text: "Passwords do not match." });
   }
 
   if (password.length < 8) {
-    res.status(400).json({
-      status: "unsuccesful",
-      message: "password must be at least 8 characters long"
-    });
-    return;
+    errors.push({ text: "Passwords muts be at least 8 characters" });
   }
 
   const userFound = await User.findOne({ email: email })
-  console.log("found: ", userFound);
   if (userFound) {
+    errors.push({ text: "Email already registered" });
+  }
+
+  if (errors.length > 0) {
     res.status(400).json({
       status: "unsuccesful",
-      message: "email already registered"
+      message: errors,
     });
     return;
   }
-
   const user = new User({
     name: name,
     email: email,
@@ -40,6 +35,59 @@ export const signup = async (req, res) => {
   res.status(200).json({
     status: "success",
     message: "user signed up"
-  })
+  });
+  return;
+}
+
+export const login = async (req, res) => {
+  const { email, password } = req.body;
+  const userFound = await User.findOne({ email: email })
+  if (!userFound) {
+    res.status(400).json({
+      status: "unsuccesful",
+      message: "email not found"
+    });
+    return;
+  }
+
+  if (! await userFound.matchPassword(password)) {
+    res.status(400).json({
+      status: "unsuccesful",
+      message: "Incorrect password"
+    });
+    return;
+  }
+
+  res.status(200).json({
+    status: "success",
+    message: "user logged in"
+  });
+  return;
+}
+
+export const logout = async (req, res) => {
+  const { email } = req.body;
+
+  //TODO: add auth system to logout user form authentications
+
+  res.status(200).json({
+    status: "success",
+    message: "user logged out"
+  });
+  return;
+};
+
+
+export const deleteUser = async (req, res) => {
+  const { email } = req.body;
+
+  //TODO: add auth system to check user is authenticated
+  const userFound = await User.deleteOne({ email: email })
+  console.log(userFound)
+
+  res.status(200).json({
+    status: "success",
+    message: "user deleted it"
+  });
   return;
 }
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -3,8 +3,14 @@ const router = Router();
 
 import {
   signup,
+  login,
+  logout,
+  deleteUser,
 } from "../controllers/auth.controllers.js";
 
 router.post('/user', signup);
+router.get('/user', login);
+router.get('/user', logout);
+router.delete('/user', deleteUser);
 
 export default router;
