2039b26dab39122c58399eb3ede278fb301e0faf
Andres <andruix93@gmail.com>
2024-02-14 11:58:46 -0600

    feat: endpoints for activities

--- src/models/activity.js
+++ src/models/activity.js
@@ -3,7 +3,8 @@ import mongoose from "mongoose";
 const ActivitySchema = new mongoose.Schema({
   title: {
     type: String,
-    required: true
+    required: true,
+    unique: true
   },
   timesDone: {
     type: Number,
--- src/routes/activities.routes.js
+++ src/routes/activities.routes.js
@@ -2,15 +2,19 @@ import { Router } from "express";
 const router = Router();
 
 import {
-  createNote,
-  listNotes,
-  deleteNote,
-  editNote,
+  createActivity,
+  listActivities,
+  getActivity,
+  deleteActivity,
+  editActivity,
+  markAsDone,
 } from "../controllers/activities.controllers.js";
 
-router.post('/note', createNote);
-router.get('/note', listNotes);
-router.delete('/note', deleteNote);
-router.patch('/note', editNote);
+router.post('/note', createActivity);
+router.get('/note', listActivities);
+router.get('/note/:id', getActivity);
+router.delete('/note/:id', deleteActivity);
+router.patch('/note/:id', editActivity);
+router.patch('/note/done/:id', markAsDone);
 
 export default router;
